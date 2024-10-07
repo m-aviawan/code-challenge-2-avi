2966c07c883d414fc037a0e107eee35d2e53b7e2
m-aviawan <mfauziavi@gmail.com>
2024-10-07 21:41:49 +0700

    tenth commit

--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -51,7 +51,7 @@ const Testimonials = () => {
                     strokeLinejoin="round">  
                     <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" />
                   </svg>
-                  <p className='text-gray-500 lg:text-left text-right'>{item.date}</p>
+                  <p className='text-gray-800 text-sm lg:text-left text-right'>{item.date}</p>
                 </span>
               </article>
               )
